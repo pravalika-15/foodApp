bec7e0a540f4ddcc3492a1e43f005179234a3882
pravalika <pravalikaattada15@gmail.comgit config listgit config --list>
2023-01-20 19:47:36 +0530

    d

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,22 +1,46 @@
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../Store/card-context";
 import classes from "./CartButton.module.css";
-import { useContext } from "react";
-const CartButton = (props) => {
-  const cartctx = useContext(CartContext);
-  const noofCartItems = cartctx.items.reduce((curNumber, item) => {
+import { useContext, useEffect, useState } from "react";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
-  // reduce() - allows to transform an array of data to a single value
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your cart</span>
-      <span className={classes.badge}>{noofCartItems}</span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
 
-export default CartButton;
+export default HeaderCartButton;
